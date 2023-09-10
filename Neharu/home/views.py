c076799bd3096c7ec34c3f9b5717ad45ca56a2f2
nitinco <sharmanitin96644@gmail.com>
2023-09-10 13:44:36 +0530

    m

--- Neharu/home/views.py
+++ Neharu/home/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+
 
 # Create your views here.
 def home(request):
     #return HttpResponse("hello welcome to my website")
-    template = loader.get_template('test.html')
-    return HttpResponse(template.render())
+    return render(request, "test.html")
     
