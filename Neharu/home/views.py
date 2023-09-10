e4cb947c74f7a5bc249d590eab318e21f9a2885b
nitinco <sharmanitin96644@gmail.com>
2023-09-10 18:36:45 +0530

    m

--- Neharu/home/views.py
+++ Neharu/home/views.py
@@ -4,6 +4,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("hello welcome to my website")
+    #return HttpResponse("hello welcome to my website")
+    context={
+        "topic":"neharu public school",
+    }
+    return render(request, 'test.html', context)
     
     
